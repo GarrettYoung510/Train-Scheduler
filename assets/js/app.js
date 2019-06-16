91c1a439f5902970c0a5f30c3d452cf6a6083f4d
Garrett Young <garrettyoung510@gmail.com>
2019-06-16 16:31:44 -0700

    added pseudocode so I can reference in the future

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,3 +1,4 @@
+// firebase info
 const firebaseConfig = {
     apiKey: "AIzaSyA31MYUswwsnUSoU5LMxgzoyqWMurbyuGw",
     authDomain: "train-scheduler-72b1b.firebaseapp.com",
@@ -8,19 +9,22 @@ const firebaseConfig = {
     appId: "1:286718348948:web:2ed25d5c69ca046e"
 };
 
-
-firebase.initializeApp(firebaseConfig);
 // initialize app
+firebase.initializeApp(firebaseConfig);
+
 
 // variable to reference the database
 const trainData = firebase.database();
 
+// triggers on click event for submit button
 $('#submit').on('click',function(){
+    // pull value and trim data that was inputted
     let trainName = $('#train-name').val().trim();
     let destination = $('#train-destination').val().trim();
     let firstTrain = moment($('#train-time').val().trim(), "HH:mm").subtract(10,"years").format("X");
     let frequency = $('#train-frequency').val().trim();
 
+// create a const object for train
 const newTrain = {
     name: trainName,
     destination: destination,
@@ -28,32 +32,42 @@ const newTrain = {
     frequency: frequency,
 }
 
+// push newTrain data to train firebase
 trainData.ref().push(newTrain);
 
+// alert user that the train has been added
 alert("Your Train has been added");
 
+// pull values for each of these
 $('#train-name').val("");
 $('#train-destination').val("");
 $('#train-time').val("");
 $('#train-frequency').val("");
 
+// return false so this will stop running
 return false;
 })
 
-// pull data from firebase
+// pull data from firebase that was inputted before 
 trainData.ref().on("child_added", function(snapshot){
+
+    // variables for holding the snapshots of the data
     let name = snapshot.val().name;
     let destination = snapshot.val().destination;
     let frequency = snapshot.val().frequency;
     let firstTrain = snapshot.val().firstTrain;
 
+    // use moment to convert to unix for calculations finding the remainder of frequency
     let remainder = moment().diff(moment.unix(firstTrain),"minutes")%frequency;
+    // subtract remainder from frequency
     let minutes = frequency - remainder;
+    // display arrival in minutes
     let arrival = moment().add(minutes, "m").format("hh:mm A");
 
-    console.log(remainder);
-    console.log(minutes);
-    console.log(arrival);
+    // console logs to check if displaying properly
+    // console.log(remainder);
+    // console.log(minutes);
+    // console.log(arrival);
 
     $("#trainSchedule > tBody").append(`<tr><td>${name}</td><td>${destination}</td><td>${frequency}</td><td>${arrival}</td><td>${minutes}</td></tr>`);
 
